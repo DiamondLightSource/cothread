DiamondLightSource/cothread

commit 438faab1b7c99815572f75ab8d35058fcca5f52b

Cothread:  * Remove all default arguments from internal methods.  This should save the    allocation of argument dictionaries, and actually leaves the code a little    bit clearer.  * Make wait_until() now unconditionally suspend.  This fits the program logic    rather better, though it does entail pushing a couple of timeout checks out    to the caller: this affects SleepUntil() and _WaitUntil().  However, given    the previous fix to coselect this makes more sense.
git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/python/cothread@28616 e099a375-04f9-0310-9d5f-a741eaff62e1

cothread/cothread.py
@@ -164,7 +164,7 @@ class _WakeupQueue(object):
     def append(self, waiter):
         self.__waiters.append(waiter)
 
-    def wake(self, wake_all=False):
+    def wake(self, wake_all):
         if self.__waiters:
             if wake_all:
                 for task in self.__waiters:
@@ -407,22 +407,15 @@ class _Scheduler(object):
     def do_yield(self, until):
         '''Hands control to the next task with work to do, will return as
         soon as there is time.'''
-        self.wait_until(until,
-            suspend_queue = self.__yield_queue, force_yield = True)
+        self.wait_until(until, self.__yield_queue, None)
 
-    def wait_until(self, until,
-            suspend_queue = None, wakeup = None, force_yield = False):
+    def wait_until(self, until, suspend_queue, wakeup):
         '''The calling task is suspended.  If a deadline is given then the
         task will definitely be woken up when the deadline is reached if not
         before.  If a suspend_queue is given then the task is added to it
         (and it is the caller's responsibility to ensure the task is woken
         up, with a call to wakeup()).
             Returns True iff the wakeup is from a timeout.'''
-        # If the timeout has already expired then do nothing at all unless
-        # we're actually forcing a yield here.
-        if until is not None and not force_yield and time.time() >= until:
-            return True
-            
         # If no wakeup has been specified, create one.  This is a key
         # component for ensuring consistent behaviour of the system: the
         # wakeup object ensures each task is only woken up exactly once.
@@ -467,8 +460,7 @@ class _Scheduler(object):
         # It's vital to yield during this call, even if we have actually
         # timed out -- otherwise the wakeup we've just added to the poll
         # queue will get processed when it's no longer valid (oops).
-        reason = self.wait_until(until,
-            wakeup = poller.wakeup, force_yield = True)
+        reason = self.wait_until(until, None, poller.wakeup)
 
 
     def __Wakeup(self, queue, until):
@@ -548,15 +540,17 @@ class EventBase(object):
         # simulated.
         self.__wait_abort = 0
 
-    def _WaitUntil(self, timeout=None):
+    def _WaitUntil(self, timeout):
         '''Suspends the calling task until _Wakeup() is called.  Raises an
         exception if a timeout occurs first.'''
         # The scheduler tells us whether we were resumed on a timeout or on a
         # normal schedule event.
-        if _scheduler.wait_until(Deadline(timeout), self.__wait_queue):
-            raise Timedout('Timed out waiting for event')
+        deadline = Deadline(timeout)
+        if deadline is None or time.time() < deadline:
+            if _scheduler.wait_until(deadline, self.__wait_queue, None):
+                raise Timedout('Timed out waiting for event')
 
-    def _Wakeup(self, wake_all = True):
+    def _Wakeup(self, wake_all):
         '''Wakes one or all waiting tasks.  Returns False if an aborted wait
         needs to be emulated.'''
         if self.__wait_abort and not wake_all:
@@ -618,7 +612,7 @@ class Spawn(EventBase):
                     'raised uncaught exception'
                 traceback.print_exc()
                 self.__result = (True, None)
-        if not self._Wakeup():
+        if not self._Wakeup(True):
             # Aborted wakeup: consume the result now
             del self.__result
         # See wait_until() for an explanation of this return value.
@@ -930,11 +924,12 @@ _scheduler_thread_id = thread.get_ident()
 def SleepUntil(deadline):
     '''Sleep until the specified deadline.  Note that if the deadline has
     already passed then no yield of control will occur.'''
-    _scheduler.wait_until(deadline)
+    if time.time() < deadline:
+        _scheduler.wait_until(deadline, None, None)
 
 def Sleep(timeout):
     '''Sleep until the specified timeout has expired.'''
-    _scheduler.wait_until(time.time() + timeout)
+    SleepUntil(Deadline(timeout))
 
 def Yield(timeout = 0):
     '''Hands control back to the scheduler.  Control is returned either after

commit f85a45bf11a56985e4f5aecee056fa261564b7c3

Cothread:  * Fix rather unfortunate bug in timeouts introduced in r28616 rework: wrong    logic for already expired timeouts!  * Add connect() method to catools library.
git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/python/cothread@31962 e099a375-04f9-0310-9d5f-a741eaff62e1

cothread/catools.py
@@ -29,7 +29,7 @@
 
 '''Pure Python ctypes interface to EPICS libca Channel Access library
 
-Supports three methods:
+Supports the following methods:
 
     caget(pvs, ...)
         Returns a single snapshot of the current value of each PV.
@@ -39,6 +39,9 @@ Supports three methods:
 
     camonitor(pvs, callback, ...)
         Receive notification each time any of the listed PVs changes.
+
+    connect(pvs, ...)
+        Can be used to establish PV connection before using the PV.
 
 See the documentation for the individual functions for more details on using
 them.'''
@@ -57,6 +60,7 @@ __all__ = [
     'caput',            # Write PVs to channel access
     'caget',            # Read PVs from channel access
     'camonitor',        # Monitor PVs over channel access
+    'connect',          # Establish PV connection
 
     # Basic DBR request codes: any one of these can be used as part of a
     # datatype request.  
@@ -561,7 +565,7 @@ def caget(pvs, **kargs):
         timeout = 5, datatype = None,
         format = FORMAT_RAW, count = 0, throw = True)
 
-    Retrieves the value from one or more PVs.  If a single pv is given then
+    Retrieves the value from one or more PVs.  If a single PV is given then
     a single value is returned, otherwise a list of values is returned.
 
     Every value returned has the following fields:
@@ -771,6 +775,53 @@ def caput(pvs, values, **kargs):
         return caput_array(pvs, values, **kargs)
 
 
+
+# ----------------------------------------------------------------------------
+#   connect
+
+@maybe_throw
+def connect_one(pv, wait = True, timeout = 5):
+    channel = _channel_cache[pv]
+    if wait:
+        channel.Wait(timeout)
+    return ca_nothing(pv)
+    
+
+def connect_array(pvs, **kargs):
+    return cothread.WaitForAll([
+        cothread.Spawn(connect_one, pv, raise_on_wait = True, **kargs)
+        for pv in pvs])
+    
+
+def connect(pvs, **kargs):
+    '''connect(pvs, wait = True, timeout = 5, throw = True)
+
+    Establishes a connection to one or more PVs.  A single PV or a list of PVs
+    can be given.  This does not normally need to be called, as the ca...()
+    routines will establish their own connections as required, but after a
+    successful connection we can guarantee that caput(..., wait=False) will
+    complete immediately without suspension.
+
+    The following arguments affect the behaviour of connect as follows:
+
+    wait
+        Normally the connect routine will not return until the requested
+        connection is established.  If wait=False is set then a connection
+        request will be queued and connect will unconditionally succeed.
+
+    timeout
+        How long to wait for the connection to be established.
+
+    throw
+        Normally an exception will be raised if the channel cannot be
+        connected to.  If this is set to False then instead for each failing
+        PV a sentinel value with .ok == False is returned.
+    '''
+    if isinstance(pvs, str):
+        return connect_one(pvs, **kargs)
+    else:
+        return connect_array(pvs, **kargs)
+    
 
 # ----------------------------------------------------------------------------
 #   Final module initialisation

cothread/cothread.py
@@ -543,12 +543,13 @@ class EventBase(object):
     def _WaitUntil(self, timeout):
         '''Suspends the calling task until _Wakeup() is called.  Raises an
         exception if a timeout occurs first.'''
-        # The scheduler tells us whether we were resumed on a timeout or on a
-        # normal schedule event.
         deadline = Deadline(timeout)
-        if deadline is None or time.time() < deadline:
-            if _scheduler.wait_until(deadline, self.__wait_queue, None):
-                raise Timedout('Timed out waiting for event')
+        # If the deadline has already expired don't call into the scheduler:
+        # as a matter of policy, we don't lose control in this case.
+        # Otherwise the scheduler will tell us if we've timed out.
+        if (deadline is not None and time.time() >= deadline) or \
+                _scheduler.wait_until(deadline, self.__wait_queue, None):
+            raise Timedout('Timed out waiting for event')
 
     def _Wakeup(self, wake_all):
         '''Wakes one or all waiting tasks.  Returns False if an aborted wait
@@ -924,7 +925,7 @@ _scheduler_thread_id = thread.get_ident()
 def SleepUntil(deadline):
     '''Sleep until the specified deadline.  Note that if the deadline has
     already passed then no yield of control will occur.'''
-    if time.time() < deadline:
+    if deadline is None or time.time() < deadline:
         _scheduler.wait_until(deadline, None, None)
 
 def Sleep(timeout):

tests/interactive.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python2.4
+
 import os
 
+import require
 from cothread import *
 from cothread.coselect import *
 
@@ -8,6 +11,7 @@ def reader(r):
     while True:
         ll = poll_list(((r, POLLIN),))
         if ll:
+#            if ll[r] & POLLIN:
             if ll[0][1] & POLLIN:
                 l = os.read(r, 1024)
                 print 'reader read', repr(l)
@@ -26,7 +30,7 @@ def reader2(r):
 
 
 r, w = os.pipe()
-Spawn(reader2, r)
+Spawn(reader, r)
 os.write(w, 'testing')
 
 select([0], [], [], 1)