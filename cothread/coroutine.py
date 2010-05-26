5a05b62a5b3f96eed6dbeadfa279d069043374cc
mga83 <mga83@e099a375-04f9-0310-9d5f-a741eaff62e1>
2010-05-26 06:43:18 +0000

    Cothread: Allow coroutine import to fall back to greenlet if appropriate.
    If _coroutine.so cannot be loaded try loading greenlet instead.
    
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/python/cothread@45681 e099a375-04f9-0310-9d5f-a741eaff62e1

--- cothread/coroutine.py
+++ cothread/coroutine.py
@@ -3,49 +3,73 @@
 import ctypes
 import os
 
-_coroutine = ctypes.cdll.LoadLibrary(
-    os.path.join(os.path.dirname(__file__), '_coroutine.so'))
+try:
+    _coroutine = ctypes.cdll.LoadLibrary(
+        os.path.join(os.path.dirname(__file__), '_coroutine.so'))
 
-get_current = _coroutine.get_current_coroutine
-delete = _coroutine.delete_coroutine
+except:
+    # Can't load _coroutine library, try using greenlet in one form or another
+    # instead.  Odd.  Two different versions of the greenlet library with
+    # slightly different interfaces.
+    try:
+        import greenlet
+        create_greenlet = greenlet.greenlet
+    except ImportError:
+        from py.magic import greenlet
+        create_greenlet = greenlet
 
-_coroutine_action = ctypes.CFUNCTYPE(
-    ctypes.py_object, ctypes.py_object, ctypes.py_object)
+    get_current = greenlet.getcurrent
 
-_create_coroutine = _coroutine.create_coroutine
-_create_coroutine.argtypes = [
-    ctypes.c_void_p, _coroutine_action, ctypes.c_int, ctypes.py_object]
-_create_coroutine.restype = ctypes.c_void_p
+    def create(parent, action, stack_size):
+        return create_greenlet(action, parent)
 
-_switch_coroutine = _coroutine.switch_coroutine
-_switch_coroutine.argtypes = [ctypes.c_void_p, ctypes.py_object]
-_switch_coroutine.restype = ctypes.py_object
+    def switch(coroutine, arg):
+        return coroutine.switch(arg)
 
-ctypes.pythonapi.Py_IncRef.argtypes = [ctypes.py_object]
-ctypes.pythonapi.Py_DecRef.argtypes = [ctypes.py_object]
+    def delete(coroutine):
+        pass
+
+else:
+    # _coroutine successfully loaded, proceed with wrapping it.
+    get_current = _coroutine.get_current_coroutine
+    delete = _coroutine.delete_coroutine
+
+    _coroutine_action = ctypes.CFUNCTYPE(
+        ctypes.py_object, ctypes.py_object, ctypes.py_object)
+
+    _create_coroutine = _coroutine.create_coroutine
+    _create_coroutine.argtypes = [
+        ctypes.c_void_p, _coroutine_action, ctypes.c_int, ctypes.py_object]
+    _create_coroutine.restype = ctypes.c_void_p
+
+    _switch_coroutine = _coroutine.switch_coroutine
+    _switch_coroutine.argtypes = [ctypes.c_void_p, ctypes.py_object]
+    _switch_coroutine.restype = ctypes.py_object
+
+    ctypes.pythonapi.Py_IncRef.argtypes = [ctypes.py_object]
+    ctypes.pythonapi.Py_DecRef.argtypes = [ctypes.py_object]
+
+    def switch(coroutine, arg):
+        # The story here with reference counting is a little delicate.  The
+        # argument is effectively transposed into a result _switch_coroutine,
+        # which means that we need to add an extra reference count.
+        ctypes.pythonapi.Py_IncRef(arg)
+        return _switch_coroutine(coroutine, arg)
+
+    @_coroutine_action
+    def _action_wrapper(action, arg):
+        # Pick up arg and switch back.  Something like this anyway.  Important
+        # thing is to only create one instance of action_wrapper to avoid memory
+        # leaks from within ctypes.
+        result = action(arg)
+        ctypes.pythonapi.Py_DecRef(action)
+        return result
+
+    def create(parent, action, stack_size):
+        ctypes.pythonapi.Py_IncRef(action)
+        return _coroutine.create_coroutine(
+            parent, _action_wrapper, stack_size, action)
 
 
-def switch(coroutine, arg):
-    # The story here with reference counting is a little delicate.  The argument
-    # is effectively transposed into a result _switch_coroutine, which means
-    # that we need to add an extra reference count.
-    ctypes.pythonapi.Py_IncRef(arg)
-    import sys
-    return _switch_coroutine(coroutine, arg)
-
-@_coroutine_action
-def _action_wrapper(action, arg):
-    # Pick up arg and switch back.  Something like this anyway.
-    # Important thing is to only create one instance of action_wrapper to avoid
-    # memory leaks from within ctypes.
-    result = action(arg)
-    ctypes.pythonapi.Py_DecRef(action)
-    return result
-
-def create(parent, action, stack_size):
-    ctypes.pythonapi.Py_IncRef(action)
-    return _coroutine.create_coroutine(
-        parent, _action_wrapper, stack_size, action)
-
-
+# Ignored for greenlet, but needs to be defined anyway.
 DEFAULT_STACK_SIZE = (1 << 16)
