921bf521519728282bd37972aed8a8a3a516a870
mga83 <mga83@e099a375-04f9-0310-9d5f-a741eaff62e1>
2010-06-08 08:57:29 +0000

    Cothread: Convert scope_epics.py to work with Qt4.
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/python/cothread@46017 e099a375-04f9-0310-9d5f-a741eaff62e1

--- examples/scope_epics.py
+++ examples/scope_epics.py
@@ -1,52 +1,34 @@
-#!/usr/bin/env dls-python
+#!/usr/bin/env dls-python2.6
 
 '''Form Example with Monitor'''
 
-import subprocess
-import os
+import os, sys
 
-# version control
 import require
 from cothread.catools import *
 from cothread import *
+from numpy import *
+from PyQt4 import Qwt5, QtGui, QtCore, uic
+
 iqt(use_timer = True)
 
-from numpy import *
-from qwt.qplt import Plot, Curve, Bottom, QwtPlot, \
-     QwtPlotCanvas, Qt, Pen, QwtDoubleRect
-
-from qt import *
-
-
-def load(filename, cls):
-    '''loads python class from ui file'''
-    pyuic = subprocess.Popen(
-        ['pyuic2.4', filename],
-        stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-    (stdout, stderr) = pyuic.communicate()
-    if pyuic.returncode != 0:
-        raise Exception(stderr)
-
-    ns = {}
-    exec stdout in ns
-    globals()[cls] = ns[cls]
-
-
-
-# Qt designer form class
+# Qt designer form class (widget is actually QtGui.QWidget)
 scope_ui_file = os.path.join(os.path.dirname(__file__), 'scope.ui')
-load(scope_ui_file, 'Scope')
+Ui_Scope, widget = uic.loadUiType(scope_ui_file)
+
 
 # subclass form to implement buttons
-class MyScope(Scope):
+class MyScope(widget, Ui_Scope):
     '''application class'''
     def __init__(self):
-        Scope.__init__(self)
+        widget.__init__(self)
+        self.setupUi(self)
+
         self.channel.setText('SR21C-DI-EBPM-01:FR:WFX')
         self.monitor = None
         # make any contents fill the empty frame
-        grid = QVBoxLayout(self.axes)
-        grid.setAutoAdd(True)
+        grid = QtGui.QGridLayout(self.axes)
+        self.axes.setLayout(grid)
         self.makeplot()
 
     def bConnect_clicked(self):
@@ -57,40 +39,42 @@ class MyScope(Scope):
             self.monitor.close()
         # connect new channel
         self.monitor = camonitor(name, self.on_event)
+        QtGui.QMessageBox.warning(self, "I'm a warning", "Warning text")
 
     def on_event(self, value):
         '''camonitor callback'''
         if value.ok:
             x = arange(value.shape[0])
-            self.p.setCurveData(1, x, value)
+            self.c.setData(x, value)
 
     def makeplot(self):
         '''set up plotting'''
         # draw a plot in the frame
-        p = Plot(self.axes, Curve([], [], 'FR:WFX', Pen(Qt.blue)))
+        p = Qwt5.QwtPlot(self.axes)
+        c = Qwt5.QwtPlotCurve('FR:WFX')
+        c.attach(p)
+        c.setPen(QtGui.QPen(QtCore.Qt.blue))
+
         # === Plot Customization ===
-        # turn off grid
-        p.enableGridX(False)
-        p.enableGridY(False)
         # set background to black
-        p.setCanvasBackground(Qt.black)
+        p.setCanvasBackground(QtCore.Qt.black)
         # stop flickering border
-        p.canvas().setFocusIndicator(QwtPlotCanvas.NoFocusIndicator)
-        # move legend to bottom
-        p.setLegendPosition(QwtPlot.Bottom)
+        p.canvas().setFocusIndicator(Qwt5.QwtPlotCanvas.NoFocusIndicator)
         # set zoom colour
-        for z in p.zoomers:
-            z.setRubberBandPen(QPen(Qt.white))
+#         for z in p.zoomers:
+#             z.setRubberBandPen(QtGui.QPen(QtCore.Qt.white))
         # set fixed scale
-        p.setAxisScale(QwtPlot.yLeft, -1e7, 1e7)
-        p.setAxisScale(QwtPlot.xBottom, 0, 2500)
+        p.setAxisScale(Qwt5.QwtPlot.yLeft, -1e7, 1e7)
+        p.setAxisScale(Qwt5.QwtPlot.xBottom, 0, 2500)
         # automatically redraw when data changes
         p.setAutoReplot(True)
         # reset plot zoom (the default is 1000 x 1000)
-        for z in p.zoomers:
-            z.setZoomBase()
-        # === End Plot Customization ===
+#         for z in p.zoomers:
+#             z.setZoomBase()
+
         self.p = p
+        self.c = c
+        self.axes.layout().addWidget(self.p)
 
 # create and show form
 s = MyScope()
