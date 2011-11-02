f33cdfe36764743ce1c2b396d4473ffaf8b15040
mga83 <mga83@e099a375-04f9-0310-9d5f-a741eaff62e1>
2011-11-02 10:58:05 +0000

    Cothread: Add select_hook() function
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/python/cothread@68990 e099a375-04f9-0310-9d5f-a741eaff62e1

--- cothread/coselect.py
+++ cothread/coselect.py
@@ -51,7 +51,21 @@ __all__ = [
     'POLLNVAL',         # Invalid request, not open.
 
     'POLLEXTRA',        # If any of these are set there is a socket problem
+
+    'select_hook',      # Replaces select methods
 ]
+
+# Pick up copies of the select methods we might modify when hooking.
+_select_select = _select.select
+_select_poll = _select.poll
+
+
+def select_hook():
+    '''Replaces the blocking methods in the select module with the non-blocking
+    methods implemented here.  Not safe to call if other threads need the
+    original methods.'''
+    _select.select = select
+    _select.poll = poll
 
 
 # A helpful routine to ensure that our select() behaves as much as possible
@@ -89,7 +103,7 @@ def poll_block_poll(poll_list, timeout = None):
     useful functionality.  This will block non-cooperatively, so should only
     be used in a scheduler loop.
         Note that the timeout is in seconds.'''
-    p = _select.poll()
+    p = _select_poll()
     for file, events in poll_list:
         p.register(file, events)
     if timeout is not None:
@@ -122,7 +136,7 @@ def poll_block_select(poll_list, timeout = None):
 
     result = {}
     try:
-        selected = _select.select(*selects + (timeout,))
+        selected = _select_select(*selects + (timeout,))
     except _select.error:
         # Oh dear.  *Something* is wrong, but I don't know which file handle
         # is broken.  This is not good: going to have to probe each file in
@@ -133,7 +147,7 @@ def poll_block_select(poll_list, timeout = None):
                 if events & event:
                     wtd.append(file)
             try:
-                selected = _select.select(*selects + (0,))
+                selected = _select_select(*selects + (0,))
             except _select.error:
                 # Still don't really know what's wrong, but it's a safe bet
                 # the problem is the file handle.
