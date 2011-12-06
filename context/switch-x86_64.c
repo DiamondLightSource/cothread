ce6cbae3fb508325b006c61d686098e3505fc132
mga83 <mga83@e099a375-04f9-0310-9d5f-a741eaff62e1>
2011-12-06 12:45:38 +0000

    Cothread: A first stab at WIN64 support
    Completely untested, but now we have at least the basic framework for
    64-bit windows support for coroutine switching.  A minor complication is
    the completely incompatible register ABI, a bigger annoyance is the need
    to treat MMX registers as non-volatile.
    
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/python/cothread@71314 e099a375-04f9-0310-9d5f-a741eaff62e1

--- context/switch-x86_64.c
+++ context/switch-x86_64.c
@@ -27,31 +27,63 @@
  *      michael.abbott@diamond.ac.uk
  */
 
-/* Coroutine frame switching for 64-bit x86
+/* Coroutine frame switching for 64-bit x86 on both Unix (Linux and OSX) and
+ * Windows.  Unfortunately the ABIs for Unix and for Windows are rather
+ * different in a number of key registers.
+ *
+ * References:
+ *      System V Application Binary Interface, AMD64 Architecture Processor
+ *      Supplement, Draft Version 0.99.5, September 3, 2010
+ *
+ *      http://msdn.microsoft.com/en-US/library/ms235286(v=VS.80).aspx
  *
  * The AMD64 architecture provides 16 general 64-bit registers together with 16
- * 128-bit SSE registers and 8 80-bit x87 floating point registers.
+ * 128-bit SSE registers, overlapping with 8 legacy 80-bit x87 floating point
+ * registers.
+ *
+ *              Both                Unix only           Windows only
+ *              ----                ---------           ------------
+ *  rax         Result register
+ *  rbx         Must be preserved
+ *  rcx                             Fourth argument     First argument
+ *  rdx                             Third argument      Second argument
+ *  rsp         Stack pointer, must be preserved
+ *  rbp         Frame pointer, must be preserved
+ *  rsi                             Second argument     Must be preserved
+ *  rdi                             First argument      Must be preserved
+ *  r8                              Fifth argument      Third argument
+ *  r9                              Sixth argument      Fourth argument
+ *  r10-r11     Volatile
+ *  r12-r15     Must be preserved
+ *  xmm0-5      Volatile
+ *  xmm6-15                         Volatile            Must be preserved
+ *  fpcsr       Non volatile
+ *  mxcsr       Non volatile
+ *
+ * Thus for the two architectures we get slightly different lists of registers
+ * to preserve.
  *
  * Registers "owned" by caller:
- *   rbx, rsp, rbp, r12-r15, mxcsr (control bits), x87 CW
- * The callee must preserve the mxcsr control bits and the x87 control word.
- * All other registers are volatile and do not need to be preserved.
+ *  Unix:       rbx, rsp, rbp, r12-r15, mxcsr (control bits), x87 CW
+ *  Windows:    rbx, rsp, rbp, rsi, rdi, r12-r15, xmm6-15
  *
- * General registers and their roles:
- *
- *   rax     Result register
- *   rbx     Must be preserved
- *   rcx     Fourth argument
- *   rdx     Third argument
- *   rsp     Stack pointer, must be preserved
- *   rbp     Frame pointer, must be preserved
- *   rsi     Second argument
- *   rdi     First argument
- *   r8      Fifth argument
- *   r9      Sixth argument
- *   r10-r11 Scratch
- *   r12-r15 Must be preserved
- */
+ * The status of mxcsr and fpcsr is a little more delicate, but it's safest to
+ * save and restore them as well here.  To be precise, the Unix ABI specifies
+ * that the control bits of mxcsr and fpcsr must be preserved by the caller,
+ * whereas the Windows ABI presents a more involved story.  In practice it is
+ * safest to save and restore them across coroutine switches. */
+
+
+/* Define the three platform dependent parameter registers. */
+#ifdef WIN64
+#define P1      "%rcx"
+#define P2      "%rdx"
+#define P3      "%r8"
+#else
+#define P1      "%rdi"
+#define P2      "%rsi"
+#define P3      "%rdx"
+#endif
 
 
 __asm__(
@@ -61,9 +93,9 @@ __asm__(
 FNAME(switch_frame)
 
 // Arguments:
-//   rdi     Address to store saved stack after switch
-//   rsi     New stack pointer
-//   rdx     Argument to pass through to switched frame
+//   P1      Address to store saved stack after switch
+//   P2      New stack pointer
+//   P3      Argument to pass through to switched frame
         // Push all the registers we need to save
 "       pushq   %rbp\n"
 "       pushq   %r15\n"
@@ -78,55 +110,128 @@ FNAME(switch_frame)
 "       fnstcw  4(%rsp)\n"              // Save x86 control word
 "       stmxcsr (%rsp)\n"               // Save MMX control word
 
+#ifdef WIN64
+"       pushq   %rsi\n"
+"       pushq   %rdi\n"
+
+        // Save MMX registers
+"       subq    $128, %rsp\n"
+"       movaps  %xmm6, 112(%rsp)\n"
+"       movaps  %xmm7, 96(%rsp)\n"
+"       movaps  %xmm8, 80(%rsp)\n"
+"       movaps  %xmm9, 64(%rsp)\n"
+"       movaps  %xmm10, 48(%rsp)\n"
+"       movaps  %xmm11, 32(%rsp)\n"
+"       movaps  %xmm12, 16(%rsp)\n"
+"       movaps  %xmm13, (%rsp)\n"
+"       subq    $32, %rsp\n"
+"       movaps  %xmm14, 16(%rsp)\n"
+"       movaps  %xmm15, (%rsp)\n"
+#endif
+
         // Switch frame and save current frame
-"       movq    %rsp, (%rdi)\n"
-"       movq    %rsi, %rsp\n"
+"       movq    %rsp, ("P1")\n"
+"       movq    "P2", %rsp\n"
+
+#ifdef WIN64
+        // Restore MMX regs
+"       movaps  (%rsp), %xmm15\n"
+"       movaps  16(%rsp), %xmm14\n"
+"       addq    $32, %rsp\n"
+"       movaps  (%rsp), %xmm13\n"
+"       movaps  16(%rsp), %xmm12\n"
+"       movaps  32(%rsp), %xmm11\n"
+"       movaps  48(%rsp), %xmm10\n"
+"       movaps  64(%rsp), %xmm9\n"
+"       movaps  80(%rsp), %xmm8\n"
+"       movaps  96(%rsp), %xmm7\n"
+"       movaps  112(%rsp), %xmm6\n"
+"       addq    $128, %rsp\n"
+
+"       popq    %rdi\n"
+"       popq    %rsi\n"
+#endif
 
         // Restore FP and MMX
 "       ldmxcsr (%rsp)\n"
 "       fldcw   4(%rsp)\n"
 "       addq    $8, %rsp\n"
 
-        // Return to caller with argument in hand
-"       movq    %rdx, %rax\n"
 "       popq    %rbx\n"
 "       popq    %r12\n"
 "       popq    %r13\n"
 "       popq    %r14\n"
 "       popq    %r15\n"
 "       popq    %rbp\n"
+
+        // Return to caller with argument in hand
+"       movq    "P3", %rax\n"
 "       ret\n"
 FSIZE(switch_frame)
 
 
 // frame_t create_frame(void *stack_base, frame_action_t action, void *context)
 // Arguments:
-//   rdi     base of stack to use
-//   rsi     action routine
-//   rdx     context to pass to action routine
+//   P1      base of stack to use
+//   P2      action routine
+//   P3      context to pass to action routine
+//
+// The initial frame is a saved context which will switch via action_entry to
+// the given action routine.  The following fields are present in both ABIs:
+//
+//  -8(P1)      Second argument for action routine
+//  -16         Action routine saved
+//  -24         action_entry, start of saved coroutine stack frame
+//  -32         ebp, saved as 0 to ensure backtraces work properly
+//  -40..-72    r15..r12,rbx -- common saved registers
+//  -80         mxcsr, fpcsr
+//
+// The following extra fields are only present on Windows:
+//
+//  -88..-96    rsi, rdi
+//  -256..-112  xmm15..xmm6
 
 FNAME(create_frame)
         // Save the extra arguments needed by the new frame
-"       movq    %rdx, -8(%rdi)\n"       // Context for action routine
-"       movq    %rsi, -16(%rdi)\n"      // Action routine to call
+"       movq    "P3", -8("P1")\n"       // Context for action routine
+"       movq    "P2", -16("P1")\n"      // Action routine to call
         // Push the frame expected by switch_frame, but store 0 for %rbp.  Set
         // things up to start control at action_entry
-"       leaq    action_entry(%rip), %rdx\n"
-"       movq    %rdx, -24(%rdi)\n"
-"       movq    $0, -32(%rdi)\n"
-"       movq    %r15, -40(%rdi)\n"
-"       movq    %r14, -48(%rdi)\n"
-"       movq    %r13, -56(%rdi)\n"
-"       movq    %r12, -64(%rdi)\n"
-"       movq    %rbx, -72(%rdi)\n"
+"       leaq    action_entry(%rip), %rax\n"
+"       movq    %rax, -24("P1")\n"
+"       movq    $0, -32("P1")\n"
+"       movq    %r15, -40("P1")\n"
+"       movq    %r14, -48("P1")\n"
+"       movq    %r13, -56("P1")\n"
+"       movq    %r12, -64("P1")\n"
+"       movq    %rbx, -72("P1")\n"
         // Save floating point and MMX control registers.  For this we'd better
         // save something real, because we're saving control settings.
 "       wait\n"
-"       fnstcw  -76(%rdi)\n"
-"       stmxcsr -80(%rdi)\n"
+"       fnstcw  -76("P1")\n"
+"       stmxcsr -80("P1")\n"
+
+#ifdef WIN64
+"       movq    %rsi, -88("P1")\n"
+"       movq    %rdi, -96("P1")\n"
+"       movaps  %xmm6, -112("P1")\n"
+"       movaps  %xmm7, -128("P1")\n"
+"       subq    $128, "P1"\n"
+"       movaps  %xmm8, -16("P1")\n"
+"       movaps  %xmm9, -32("P1")\n"
+"       movaps  %xmm10, -48("P1")\n"
+"       movaps  %xmm11, -64("P1")\n"
+"       movaps  %xmm12, -80("P1")\n"
+"       movaps  %xmm13, -96("P1")\n"
+"       movaps  %xmm14, -112("P1")\n"
+"       movaps  %xmm15, -128("P1")\n"
+"       subq    $128, "P1"\n"
+#else
+"       subq    $80, "P1"\n"
+#endif
+
         // Return the new frame pointer and we're done
-"       subq    $80, %rdi\n"
-"       movq    %rdi, %rax\n"
+"       movq    "P1", %rax\n"
 "       ret\n"
 
 "action_entry:\n"
@@ -134,12 +239,12 @@ FNAME(create_frame)
         // frame.  The top of the stack is the function we're going to call, and
         // then the context it wants, our activation argument is in %rax.
         //
-        // We will call the action routine with the argument in rdi and the
-        // saved context in rsi.
-"       popq    %rdx\n"                // Action routine
-"       popq    %rsi\n"                // Context argument
-"       movq    %rax, %rdi\n"
-"       pushq   $0\n"                  // Returning not allowed!
-"       jmp     *%rdx\n"
+        // We will call the action routine with the argument in P1 and the
+        // saved context in P2.
+"       popq    %r8\n"                  // Action routine
+"       popq    "P2"\n"                 // Context argument
+"       movq    %rax, "P1"\n"
+"       pushq   $0\n"                   // Returning not allowed!
+"       jmp     *%r8\n"
 FSIZE(create_frame)
 );
--- context/switch.c
+++ context/switch.c
@@ -27,7 +27,7 @@
  *      michael.abbott@diamond.ac.uk
  */
 
-#if defined(__APPLE__)  ||  defined(WIN32)
+#if defined(__APPLE__)  ||  defined(WIN32)  ||  defined(WIN64)
     #define FNAME(name) \
         ".globl _" #name "\n_" #name ":\n"
     #define FSIZE(name)
@@ -51,59 +51,3 @@
 #else
     #error "Don't know how to support this platform"
 #endif
-
-/* For Windows support we'll have a number of problems.  Firstly, what tool are
- * we going to build with?  Think I'd prefer mingw, but we'll have to see.  This
- * may well affect the assembler syntax as well.
- *
- * Secondly, the ABIs differ.  Variable names for Windows follow the OSX
- * convention, so that's ok, and it looks as if the 32-bit x86 ABI is identical,
- * so with luck we may be able to use switch-x86.c unchanged.
- *
- * The 64-bit ABI is somewhat gratuitously incompatible on the other hand.
- *
- * According to:
- *  http://msdn.microsoft.com/en-US/library/ms235286(v=VS.80).aspx
- *
- * The register usage is a bit different from Unix:
- *
- *  rax     Result register
- *  rbx     Must be preserved
- *  rcx     First argument
- *  rdx     Second argument
- *  rsp     Stack pointer, must be preserved
- *  rbp     Must be preserved
- *  rsi     Must be preserved
- *  rdi     Must be preserved
- *  r8      Third argument
- *  r9      Fourth argument
- *  r10-r11 Volatile
- *  r12-r15 Must be preserved
- *  xmm0-5  Volatile
- *  xmm6-15 Must be preserved
- *
- * So we need to preserve:
- *  rbx, rbp, rsi, rdi, r12-r15, xmm6-xmm15.
- *
- * Also fpcsr must be preserved ... but functions should not be called with
- * modified values anyway.  Same rules for mxcsr.
- *
- * So with a little care we ought to be able to target windows with the same
- * source.
- *
- * So let's look at the differences:
- *
- *             Unix        Windows
- * Result      rax         rax         The same
- * P1          rdi         rcx
- * P2          rsi         rdx
- * P3          rdx         r8
- *
- * Saves
- *             rbx,rbp     rbx,rbp     Same
- *             r12-r15     r12-r15     Same
- *                         rsi,rdi     Windows only
- *             fpcsr,mxcsr fpcsr,mxcsr Same
- *                         xmm6-15     Windows only (and bloody huge)
- *
- * Tempting to solve the initialisation problem by zero setting the frame... */
