0f07991e68cc7b1651ea4a051cf4775158390937
mga83 <mga83@e099a375-04f9-0310-9d5f-a741eaff62e1>
2012-02-15 14:40:49 +0000

    Cothread: Add test for asynchronous callbacks
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/python/cothread@73730 e099a375-04f9-0310-9d5f-a741eaff62e1

---
+++ tests/callback.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env dls-python2.6
+
+# Test Callback mechanism
+
+import require
+import cothread
+import time
+import thread
+import numpy
+
+
+THREADS = 5
+
+signal = cothread.EventQueue()
+
+def do_signal(name, n):
+    signal.Signal((name, n))
+
+def signaller(name):
+    n = 0
+    while True:
+        n += 1
+        cothread.Callback(do_signal, name, n)
+        time.sleep(0.1 * numpy.random.random())
+
+for n in range(THREADS):
+    thread.start_new_thread(signaller, ('Thread %d' % n,))
+
+while True:
+    name, n = signal.Wait(1)
+    print 'got', name, n
