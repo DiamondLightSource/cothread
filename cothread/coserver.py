09843a864af9eaf136cf8bc176720db32dc291ea
mga83 <mga83@e099a375-04f9-0310-9d5f-a741eaff62e1>
2014-04-17 09:56:56 +0000

    Cothread: Add GPL 2 header to new coserver.py file
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/python/cothread@112084 e099a375-04f9-0310-9d5f-a741eaff62e1

--- cothread/coserver.py
+++ cothread/coserver.py
@@ -1,3 +1,22 @@
+# This file is part of the Diamond cothread library.
+#
+# Copyright (C) 2014 Michael Davidsaver, Brookhaven National Laboratory
+#
+# The Diamond cothread library is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the License,
+# or (at your option) any later version.
+#
+# The Diamond cothread library is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+#
+
 """
 cothread friendly versions of the socket servers
 from the SocketServer and BaseHTTPServer modules
