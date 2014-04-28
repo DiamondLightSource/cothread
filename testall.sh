f8bcf82350a90ec8eea9850fd8944e20ac7a1e21
mga83 <mga83@e099a375-04f9-0310-9d5f-a741eaff62e1>
2014-04-28 06:22:37 +0000

    Cothread: Tweaks to top level test script
    Assign sensible default arguments, prepare for more tests, remove
    workaround for bug now fixed, other miscellaneous tweaks.
    
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/python/cothread@112366 e099a375-04f9-0310-9d5f-a741eaff62e1

--- testall.sh
+++ testall.sh
@@ -1,5 +1,7 @@
-#!/bin/sh
-set -e -x
+#!/bin/bash
+set -e
+
+HERE="$(cd "$(dirname "$0")"; pwd)"
 
 # Build and run unittests (currently only tests/cosocket.py)
 # for several different interpreter versions
@@ -11,42 +13,54 @@ set -e -x
 # $ ./testall.sh master python2.6 python2.7
 # $ ./testall.sh python3 python3.2
 
-BRANCH=${1:-HEAD}
-shift
+# Default revision to HEAD
+if [[ $# > 0 ]]; then
+    BRANCH="$1"
+    shift
+else
+    BRANCH=HEAD
+fi
 
-echo "Test $BRANCH"
+# If no interpreter names given use python by default
+if [[ $# > 0 ]]; then
+    PYTHONS=("$@")
+else
+    PYTHONS=(python)
+fi
 
-for PY in "$@"
-do
-    echo "Test with $PY"
+
+TESTS=(cosocket)
+
+
+echo "Testing $BRANCH"
+
+for PYTHON in "${PYTHONS[@]}"; do
+    echo "Testing with $PYTHON"
     BUILD="$(mktemp -d)"
     echo "temp dir $BUILD"
 
-    trap "rm -rf $BUILD" INT HUP TERM EXIT QUIT
+    trap "rm -rf $BUILD" EXIT
 
-    git archive --prefix=src/ $BRANCH | tar -C $BUILD -xv
-
-    # gcc 4.7 issues spurious warnings about valgrind.h macros
-    #   warning: variable ‘_qzz_res’ set but not used [-Wunused-but-set-variable]
-    sed -i -e '/Werror/d' $BUILD/src/setup.py
+    cd "$HERE"
+    git archive --prefix=src/ $BRANCH | tar -C $BUILD -x
 
     install -d $BUILD/usr/lib/python
-
     export PYTHONPATH=$BUILD/usr/lib/python
 
-    (cd $BUILD/src && $PY ./setup.py install --home $BUILD/usr)
+    cd $BUILD/src
+    $PYTHON ./setup.py install --home $BUILD/usr
 
-    if $PY $BUILD/src/tests/cosocket.py
-    then
-        echo "Pass $PY"
-    else
-        echo "Fail $PY"
-        exit 1
-    fi
+    for test in "${TESTS[@]}"; do
+        if $PYTHON $BUILD/src/tests/$test.py; then
+            echo "Pass $PYTHON $test"
+        else
+            echo "Fail $PYTHON $test"
+            exit 1
+        fi
+    done
 
     rm -rf $BUILD
-    trap "" INT HUP TERM QUIT
-
+    trap '' EXIT
 done
 
 exit 0
