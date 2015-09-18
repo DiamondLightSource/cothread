f52fc2fba74ac7a10044e46daf4d0fe5ab390f3d
Tom Cobb <tom.cobb@diamond.ac.uk>
2015-09-18 12:09:54 +0100

    Added test for broken exception raising

--- tests/test_exception.py
+++ tests/test_exception.py
@@ -4,6 +4,7 @@ import unittest
 # Add cothread onto file and import
 import sys
 import os
+import subprocess
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 import cothread
 
@@ -26,5 +27,10 @@ class ExceptionTest(unittest.TestCase):
         fg.t1('foreground')
         self.assertEqual(sys.getrefcount(fg), 2)
         
+    def test_exception_type_gets_passed_through(self):
+        pid = os.getpid()
+        p = subprocess.Popen("sleep 1 && kill -2 %s" % pid, shell=True)
+        self.assertRaises(KeyboardInterrupt, cothread.Sleep, 2)
+                
 if __name__ == '__main__':
     unittest.main(verbosity=2)
