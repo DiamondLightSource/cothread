c48dc33679275e3c609a7e3fe3d47164e2792b90
Tom Cobb <tom.cobb@diamond.ac.uk>
2017-07-07 15:21:10 +0100

    Added some more ca tests

--- tests/test_catools.py
+++ tests/test_catools.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import tempfile
+import cothread
 from cothread import catools
 
 
@@ -21,26 +22,73 @@ class SoftIocTest(counittest.TestCase):
     )
     iocpost = "dbl\r"
 
+    def test_non_existant(self):
+        self.assertIOCRunning()
+
+        ne = self.testprefix+'ne'
+        with self.assertRaises(catools.ca_nothing) as cm:
+            catools.caget(ne, timeout=0.1)
+
+        self.assertEqual(
+            repr(cm.exception),
+            "ca_nothing('%s', 80)" % ne)
+        self.assertEqual(
+            str(cm.exception),
+            "%s: User specified timeout on IO operation expired" % ne)
+        self.assertFalse(bool(cm.exception))
+        with self.assertRaises(TypeError):
+            for _ in cm.exception:
+                pass
+
+    def test_monitor(self):
+        self.assertIOCRunning()
+        ai = self.testprefix + 'ai'
+
+        values = []
+
+        def callback(value):
+            values.append(value)
+
+        m = catools.camonitor(ai, callback, notify_disconnect=True)
+
+        # Wait for connection
+        while not values:
+            cothread.Sleep(0.1)
+        catools.caput(ai, 43, wait=True)
+        catools.caput(ai, 44, wait=True)
+        self.iocStop()
+
+        # Can't call iocStop twice...
+        def iocStop():
+            return
+
+        self.iocStop = iocStop
+        m.close()
+
+        assert len(values) == 4
+        assert values[:3] == [42, 43, 44]
+        assert [v.ok for v in values] == [True, True, True, False]
+
     def test_ai(self):
         # wait for CA server to start
         self.assertIOCRunning()
 
         ai = self.testprefix+'ai'
-        V = catools.caget(ai, timeout=1)
+        v = catools.caget(ai, timeout=1)
 
-        self.assertEqual(V, 42.0)
+        self.assertEqual(v, 42.0)
 
     def test_si(self):
         self.assertIOCRunning()
         si = self.testprefix+'si'
 
-        V = catools.caget(si)
-        self.assertNotEqual(V, 'hello world')
+        v = catools.caget(si)
+        self.assertNotEqual(v, 'hello world')
 
         catools.caput(si, 'hello world')
 
-        V = catools.caget(si)
-        self.assertEqual(V, 'hello world')
+        v = catools.caget(si)
+        self.assertEqual(v, 'hello world')
 
     def test_pvtree(self):
         from cothread.tools.pvtree import main
