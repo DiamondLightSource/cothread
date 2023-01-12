57daf901ab286b7cfde844d6c67d4f2d75e3f52c
Michael Abbott <michael.abbott@diamond.ac.uk>
2023-01-12 14:30:06 +0000

    Add recent Python versions to list of supported versions

--- setup.py
+++ setup.py
@@ -87,6 +87,10 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'License :: OSI Approved :: '
             'GNU General Public License v2 or later (GPLv2+)',
     ],
