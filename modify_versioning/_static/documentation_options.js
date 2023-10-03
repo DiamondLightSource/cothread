143f184c24387b65467d18a34aaecbcd6bd9f432
AlexanderWells-diamond <AlexanderWells-diamond@users.noreply.github.com>
2023-10-03 11:04:01 +0000

    deploy: 991755d6146212b77e51798c38c98f57ba6a0d41

---
+++ modify_versioning/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '2.19.2.dev2+g991755d',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: true,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
