27d806136ff75f561ec3567b8d257635b2c3d1b9
AlexanderWells-diamond <AlexanderWells-diamond@users.noreply.github.com>
2024-01-22 09:04:57 +0000

    deploy: db4f0fc0a098addaf76d4584be4a1a11930fd37e

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '2.19.2.dev3+gf179fb5',
+    VERSION: '2.19.2.dev7+gdb4f0fc',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
@@ -7,7 +7,7 @@ const DOCUMENTATION_OPTIONS = {
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: true,
+    NAVIGATION_WITH_KEYS: false,
     SHOW_SEARCH_SUMMARY: true,
     ENABLE_SEARCH_SHORTCUTS: true,
 };
