d047b5377e61d7573bea032c02f739831bc20324
AlexanderWells-diamond <AlexanderWells-diamond@users.noreply.github.com>
2025-05-19 08:58:06 +0000

    deploy: 30f539a6035bad13fc8c6f31a55afe71650950ff

---
+++ more_ci_fixes/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '2.20.1.dev12+g30f539a',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
